97eeb2d48c8991c7553668c733d0f4bbc0d0f311
victor juarez martinez <156w0196@zongolica.tecnm.mx>
2020-05-04 19:22:54 -0500

    five commit

---
+++ retrofit2/app/src/main/java/com/example/retrofit2/Modelo/PokemonRespuesta.java
@@ -0,0 +1,15 @@
+package com.example.retrofit2.Modelo;
+
+import java.util.ArrayList;
+
+public class PokemonRespuesta {
+    private ArrayList<Pokemon> results;
+
+    public ArrayList<Pokemon> getResults() {
+        return results;
+    }
+
+    public void setResults(ArrayList<Pokemon> results) {
+        this.results = results;
+    }
+}
